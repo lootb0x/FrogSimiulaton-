5aeaefea6171295ac80720dfb5cb89f9d68b8a60
lootb0x <justinlin8031@gmail.com>
2025-05-05 03:21:57 +0000

    FINALLLY DONE

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,14 +1,13 @@
+import java.util.Scanner;
+
+
 public class FrogSimulation
 {
 /** Distance, in inches, from the starting position to the goal. */
-private int goalDistance;
+private int goalDistance=0;;
 /** Maximum number of hops allowed to reach the goal. */
-private int maxHops;
-
-/** Constructs a FrogSimulation where dist is the distance, in inches, from the starting
-* position to the goal, and numHops is the maximum number of hops allowed to reach the goal.
-* Precondition: dist > 0; numHops > 0
-*/
+private int maxHops = 0;
+private Scanner s = new Scanner(System.in);
 public FrogSimulation(int dist, int numHops)
 {
 goalDistance = dist;
@@ -17,19 +16,38 @@ maxHops = numHops;
 /** Returns an integer representing the distance, in inches, to be moved when the frog hops.
 */
 private int hopDistance()
-{ /* implementation not shown */ }
+{
+    return (int)(Math.random()*21)-10;
 
-/** Simulates a frog attempting to reach the goal as described in part (a).
-* Returns true if the frog successfully reached or passed the goal during the simulation;
-* false otherwise.
-*/
+}
+
+
 public boolean simulate()
-{ /* to be implemented in part (a) */ }
+{ 
+    int start = goalDistance;
+    for(int x= 0; x<maxHops; x++)
+    {
+
+        goalDistance -= hopDistance();
+        if(goalDistance> start) return false;
+        if(goalDistance<=0) return true;
+    }
+    return false;
+
+}
 
 /** Runs num simulations and returns the proportion of simulations in which the frog
 * successfully reached or passed the goal.
 * Precondition: num > 0
 */
 public double runSimulations(int num)
-{ /* to be implemented in part (b) */ }
+{ 
+    double count =0.0;
+    for(int x =0; x<num; x++)
+    if(this.simulate() == true) count++;
+
+
+
+    return (double)count/num;
+}
 }
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args)
+    {
+    
+    
+            FrogSimulation test = new FrogSimulation(24, 5);
+
+            System.out.println(test.runSimulations(1000));
+
+    }
+}
